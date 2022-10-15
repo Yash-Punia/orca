cbbc4fd053b228757e8e9783818dc56fe46a531c
cationic-lady <57410575+cationic-lady@users.noreply.github.com>
2022-10-15 19:08:13 +0530

    added new outline button

---
+++ src/lib/components/designs/buttons/outlineButton.svelte
@@ -0,0 +1,6 @@
+<div class="flex flex-col items-center ">
+    <div class="mb-2 cursor-pointer w-fit px-4 py-1 transition-all border-2 border-white border-b-black hover:border-black ">
+        Outline Button
+    </div>
+    by Bhawakshi
+</div>
