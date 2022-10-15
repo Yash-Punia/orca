87c581a01a7b1f8a1a6d1aba726b3cb5ec3db929
cationic-lady <57410575+cationic-lady@users.noreply.github.com>
2022-10-15 18:54:38 +0530

    Added new Material desing button

--- src/lib/components/designs/buttons/index.js
+++ src/lib/components/designs/buttons/index.js
@@ -2,11 +2,14 @@ import BlueRounded from './blueRounded.svelte';
 import GreenFlat from './greenFlat.svelte';
 import PurplePlain from './purplePlain.svelte';
 import PinkWithHover from './pinkWithHover.svelte';
+import MaterialButton from './materialButton.svelte';
+
 const buttons = [
     BlueRounded,
     GreenFlat,
     PurplePlain,
-    PinkWithHover
+    PinkWithHover,
+    MaterialButton
 ]
 
 export default buttons;
---
+++ src/lib/components/designs/buttons/materialButton.svelte
@@ -0,0 +1,6 @@
+<div class="flex flex-col items-center ">
+    <div class="mb-2 cursor-pointer w-fit px-4 py-1 text-white text-md bg-blue-500 shadow-sm transition-all hover:shadow-lg active:bg-blue-700">
+        Material Button
+    </div>
+    by Bhawakshi
+</div>
