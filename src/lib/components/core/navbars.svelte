41aad0142f6a23d417d1c80deea45264b9fc6dbc
cationic-lady <57410575+cationic-lady@users.noreply.github.com>
2022-10-15 19:48:58 +0530

    added new plainGreen navbar

--- src/lib/components/core/navbars.svelte
+++ src/lib/components/core/navbars.svelte
@@ -3,7 +3,6 @@
 </script>
 
 <div class="w-2/3 mx-auto py-4">
-	<!-- <h1 class="font-body text-xl border-b-2 w-fit border-black mb-4 rounded-md">navbars</h1> -->
 	<div class="flex flex-col">
 		{#each navbars as navbar}
 			<svelte:component this={navbar}/>
